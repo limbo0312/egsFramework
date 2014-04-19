404ef287782b5f61159363a61f41cf06424033e6
Engels <limbo0312@gmail.com>
2014-04-19 22:34:14 +0800

    init thirdLib

---
+++ egsFramework/egsFramework/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  egsFramework
+//
+//  Created by Engels on 14-4-19.
+//  Copyright (c) 2014年 EGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ egsFramework/egsFramework/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  egsFramework
+//
+//  Created by Engels on 14-4-19.
+//  Copyright (c) 2014年 EGS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
