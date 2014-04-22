7d99b012bbaa6f78a7b979f83456f94f440d5387
Engels <limbo0312@gmail.com>
2014-04-22 19:53:39 +0800

    Revert 3e19c7e..57fd9f6
    This rolls back to commit 3e19c7ea7b2b93500a0aa035de9b388ee3687fa4.

--- egsFramework/egsFramework/ViewController.h
+++ egsFramework/egsFramework/ViewController.h
@@ -7,8 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "RNFrostedSidebar.h"
 
-@interface ViewController : UIViewController<RNFrostedSidebarDelegate>
+@interface ViewController : UIViewController
 
 @end
