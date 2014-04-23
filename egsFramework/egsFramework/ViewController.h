cc2c9e34ebd6c3485ac97c44573e056aec7a715d
Engels <limbo0312@gmail.com>
2014-04-23 16:47:12 +0800

    add base framework

---
+++ egsFramework/egsFramework/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  egsFramework
+//
+//  Created by Engels on 14-4-19.
+//  Copyright (c) 2014年 EGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RNFrostedSidebar.h"
+
+@interface ViewController : UIViewController<RNFrostedSidebarDelegate>
+
+@end
